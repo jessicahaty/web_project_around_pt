c9255b4da4eadbf755e6d18e23972f40583bd7b5
jessicahaty <jessicaisibasi@gmail.com>
2026-09-03 05:38:39 -0300

    parte 2

---
+++ scripts/index.js
@@ -0,0 +1,73 @@
+let initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+
+  {
+    name: "Parque Nacional Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-popup");
+const editProfileCloseButton = editProfileModal.querySelector(".popup__close");
+const profileForm = editProfileModal.querySelector("#edit-profile-form");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const nameInput = editProfileModal.querySelector(".popup__input_type_name");
+const descriptionInput = editProfileModal.querySelector(
+  ".popup__input_type_description",
+);
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editProfileModal);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+}
+
+editProfileButton.addEventListener("click", handleOpenEditModal);
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+
+editProfileCloseButton.addEventListener("click", () => {
+  closeModal(editProfileModal);
+});
+
+initialCards.forEach((card) => {
+  console.log(card.name);
+});
